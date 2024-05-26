fc01e19ffbc7516ed331fb3d92c0e0e09ea1f501
wkyuu <wkyuu@majo.im>
2024-05-26 11:31:04 +0800

    :wq

--- diy/unix_install_vim.sh
+++ diy/unix_install_vim.sh
@@ -1,24 +1,31 @@
-#env bash
+#!/usr/bin/env bash
 
 dirs=(
-    $HOME/.vim $HOME/.vim/tmp $HOME/.vim/autoload $HOME/.vim/colors
+    $HOME/.vim $HOME/.vim/tmp $HOME/.vim/autoload $HOME/.vim/plugins
 )
 
-do for dir in ${dirs[@]}; do
+for dir in ${dirs[@]}; do
     if [ ! -d $dir ]; then
         mkdir -p $dir
     fi
 done
 
+mkdir $HOME/.vim/share/
+ln -s /usr/share/vim/* $HOME/.vim/share/
+if [ -d $HOME/.vim/colors ]; then
+    rm -rf $HOME/.vim/colors
+    ln -s $(find /usr/share/vim/ -name colors) $HOME/.vim/colors
+fi
+
+
 curl \
     -fLo ~/.vimrc \
     https://raw.githubusercontent.com/shi9uma/vim/main/diy/unix_vimrc
-
 curl \
     -fLo ~/.vim/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl \
+sudo curl \
     -fLo ~/.vim/colors/gruvbox.vim \
     --create-dirs \
     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/gruvbox.vim
@@ -35,4 +42,5 @@ curl \
     --create-dirs \
     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/lightline/colorscheme/gruvbox.vim
 
+sudo chown -R $USER:$USER $HOME/.vim
 vim -c PlugInstall
