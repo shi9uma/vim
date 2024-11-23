0dbb5207baaa6880b191855a3ff5ce214c32c3c7
shiguma <wkyuu@majo.im>
2024-11-23 19:49:29 +0800

    update 1 xia

--- diy/unix-install-vim.sh
+++ diy/unix-install-vim.sh
@@ -5,6 +5,13 @@ if [ -z "$all_proxy" ]; then
     exit 1
 fi
 
+if [ "$(id -u)" -ne 0 ]; then
+	echo "run script with sudo"
+	exit 1
+fi
+
+export all_proxy="http://192.168.9.2:7890"
+
 vim_root=/usr/share/vim
 vim_version_dir_name=$(ls $vim_root | grep '^vim[0-9]\{2\}$')
 vim_dir=$vim_root/$vim_version_dir_name
@@ -14,19 +21,17 @@ if [ ! -d $local_vim_root ]; then
     mkdir $local_vim_root
 fi
 
-sudo ln -s $vim_dir/autoload $local_vim_root/autoload
-sudo ln -s $vim_dir/colors $local_vim_root/colors
+ln -s $vim_dir/autoload $local_vim_root/autoload
+ln -s $vim_dir/colors $local_vim_root/colors
 
 mkdir -p $local_vim_root/tmp
 
-sudo chown -R $USER:$USER $local_vim_root
+chown -R $USER:$USER $local_vim_root
 
 curl \
-    -x $all_proxy \
     -fLo ~/.vimrc \
     https://raw.githubusercontent.com/shi9uma/vim/main/diy/unix-vimrc
-sudo curl \
-    -x $all_proxy \
+curl \
     -fLo $local_vim_root/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -51,4 +56,4 @@ sudo curl \
 #     --create-dirs \
 #     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/lightline/colorscheme/gruvbox.vim
 
-vim -c PlugInstall
+vim -c PlugInstall -c qa
