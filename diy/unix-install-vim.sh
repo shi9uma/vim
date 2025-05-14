4045c775c89bab03ba180d09c9b95ca9b588b4ba
sparkle <wkyuu@majo.im>
2025-05-14 13:57:38 +0800

    u1x

--- diy/unix-install-vim.sh
+++ diy/unix-install-vim.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ -z "$all_proxy" ]; then
-    echo "set \$all_proxy"
-    exit 1
-fi
+# if [ -z "$all_proxy" ]; then
+#     echo "set \$all_proxy"
+#     exit 1
+# fi
 
 if [ "$(id -u)" -ne 0 ]; then
 	echo "run script with sudo"
 	exit 1
 fi
 
-export all_proxy="http://192.168.9.2:7890"
+# export all_proxy="http://192.168.9.2:7890"
 
 vim_root=/usr/share/vim
 vim_version_dir_name=$(ls $vim_root | grep '^vim[0-9]\{2\}$')
@@ -35,25 +35,5 @@ curl \
     -fLo $local_vim_root/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# sudo curl \
-#     -x $all_proxy \
-#     -fLo $local_vim_root/colors/gruvbox.vim \
-#     --create-dirs \
-#     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/gruvbox.vim
-# sudo curl \
-#     -x $all_proxy \
-#     -fLo$local_vim_root/autoload/gruvbox.vim \
-#     --create-dirs \
-#     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/gruvbox.vim
-# sudo curl \
-#     -x $all_proxy \
-#     -fLo$local_vim_root/autoload/airline/themes/gruvbox.vim \
-#     --create-dirs \
-#     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/airline/themes/gruvbox.vim
-# sudo curl \
-#     -x $all_proxy \
-#     -fLo$local_vim_root/autoload/lightline/colorscheme/gruvbox.vim \
-#     --create-dirs \
-#     https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/lightline/colorscheme/gruvbox.vim
 
 vim -c PlugInstall -c qa
